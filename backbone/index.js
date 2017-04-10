2512faa09fb454c1a41d73ce7759e4950f4436f0
nichenqin1001 <nichenqin@hotmail.com>
2017-04-10 22:18:32 +0800

    Learn Backbone.js Tutorial by Building an App!

--- backbone/index.js
+++ backbone/index.js
@@ -1,11 +1,33 @@
-(function ($) {
-    World = Backbone.Model.extend({
-        name: null
-    });
+// Backbone Model
 
-    Worlds = Backbone.Collection.extend({
-        initialize: function (models, options) {
-            this.bind('add', options.view.addOneWorld);
-        }
-    });
-}(jQuery));
+var Blog = Backbone.Model.extend({
+    defaults: {
+        author: '',
+        title: '',
+        url: '',
+    }
+});
+
+// Backbone Collection
+
+var Blogs = Backbone.Collection.extend({
+
+});
+
+// instantiate two blogs
+
+var blog1 = new Blog({
+    author: 'Micheal',
+    title: 'Micheal\'s Blog',
+    url: 'michealsblog.com'
+});
+
+var blog2 = new Blog({
+    author: 'John',
+    title: 'John\'s Blog',
+    url: 'johnsblog.com'
+});
+
+// instantiate Collection
+
+var blogs = new Blogs([blog1, blog2]);
