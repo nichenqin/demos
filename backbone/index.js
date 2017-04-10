43e9123650527b05745f0b676cf0b14f8e3bae32
nichenqin1001 <nichenqin@hotmail.com>
2017-04-10 22:42:20 +0800

    view init

--- backbone/index.js
+++ backbone/index.js
@@ -31,3 +31,25 @@ var blog2 = new Blog({
 // instantiate Collection
 
 var blogs = new Blogs([blog1, blog2]);
+
+// Backbone View for on blog
+
+var BlogView = Backbone.View.extend({
+    model: new Blog(),
+
+    tagName: 'tr',
+
+    initialize: function () {
+        this.template = _.template($('.blogs-list-template').html());
+    },
+
+    render: function () {
+        this.$el.html(this.template({ model: this.model.toJSON() }));
+    }
+});
+
+// Backbone View for all blogs
+
+var BlogsView = Backbone.View.extend({
+
+});
