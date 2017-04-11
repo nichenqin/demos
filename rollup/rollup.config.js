528d3fbe1b7d1a0bd2b1ae064c23597d0628e9d5
nichenqin1001 <nichenqin@hotmail.com>
2017-04-11 21:47:05 +0800

    rollup init

---
+++ rollup/rollup.config.js
@@ -0,0 +1,5 @@
+export default {
+    entry: './src/scripts/main.js',
+    dest: 'build/js/main.min.js',
+    format: 'iife'
+};
---
+++ rollup/src/scripts/main.js
@@ -0,0 +1,7 @@
+import app from './app/app';
+
+var main = function () {
+    console.log(app);
+};
+
+module.exports = main;
