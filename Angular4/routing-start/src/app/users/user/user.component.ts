095bfe697f4941b975e2a41abad92512e054f11d
nichenqin1001 <nichenqin@hotmail.com>
2017-04-21 11:24:49 +0800

    router

---
+++ Angular4/routing-start/src/app/users/user/user.component.ts
@@ -0,0 +1,25 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    };
+  }
+
+  ngOnDestroy() {
+
+  }
+
+}
