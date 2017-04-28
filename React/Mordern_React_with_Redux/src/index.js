0c069a66585afa6c84f5f0057b9db33891cb38a0
nichenqin1001 <nichenqin@hotmail.com>
2017-04-28 12:14:36 +0800

    init

--- React/Mordern_React_with_Redux/src/index.js
+++ React/Mordern_React_with_Redux/src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDe9AnLzdFIqUIGkI-1xmGvu4-qI3bDh9Y';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({ key: API_KEY, term: '湘南乃风' }, videos => {
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => { this.setState({ selectedVideo }); }}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
