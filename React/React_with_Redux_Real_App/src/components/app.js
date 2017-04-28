8f436e93f182b5ff7d65861fe764e29a749a03ae
nichenqin1001 <nichenqin@hotmail.com>
2017-04-28 23:04:56 +0800

    actions

--- React/React_with_Redux_Real_App/src/components/app.js
+++ React/React_with_Redux_Real_App/src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/books';
+import BookDetail from '../containers/book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
