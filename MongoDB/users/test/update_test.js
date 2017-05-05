48d6b5c3b46e531a7e316de2984c3633e38ea2d3
nichenqin1001 <nichenqin@hotmail.com>
2017-05-05 16:51:37 +0800

    update

---
+++ MongoDB/users/test/update_test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Upadating records', () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => { done(); });
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name = 'Alex');
+      });
+    done();
+  }
+
+  // set and save
+  it('instance type using set & save', (done) => {
+    // set...
+    joe.set('name', 'Alex');
+    // ...and save
+    assertName(joe.save(), done);
+  });
+
+  // instance update
+  it('A model instance can update', (done) => {
+    assertName(joe.update({ name: 'Alex' }), done);
+  });
+
+  // class base update
+  it('A model class can update', (done) => {
+    assertName(User.update({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+
+  it('A model class can update one record', (done) => {
+    assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+
+  it('A model class can update one record an Id and update', (done) => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their postCount incremented by 1', () => {
+  });
+});
