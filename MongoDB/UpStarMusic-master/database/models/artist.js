0d73b76071ad087cc6faef6f7c317de6f7e0dac6
nichenqin1001 <nichenqin@hotmail.com>
2017-05-08 16:21:32 +0800

    set model

--- MongoDB/UpStarMusic-master/database/models/artist.js
+++ MongoDB/UpStarMusic-master/database/models/artist.js
@@ -1,1 +1,21 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const { Scheme } = mongoose;
+
+const AlbumSchema = require('./album');
+
+const ArtistSchema = new Scheme({
+  name: String,
+  age: Number,
+  yearsActive: Number,
+  image: String,
+  genre: String,
+  website: String,
+  netWorth: Number,
+  labelName: String,
+  retired: Boolean,
+  albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+
+module.exports = Artist;
