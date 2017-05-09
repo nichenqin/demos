a061f1cc035226e70c19674519ab68c013394663
nichenqin1001 <nichenqin@hotmail.com>
2017-05-09 21:01:14 +0800

    express init

---
+++ MongoDB/muber/routes/routes.js
@@ -0,0 +1,9 @@
+const DriversController = require('../controllers/drivers_controller');
+
+module.exports = (app) => {
+
+  // watch for incoming request of method GET
+  // to the route http://localhost:3050/api
+  app.get('/api', DriversController.greeting);
+
+};
