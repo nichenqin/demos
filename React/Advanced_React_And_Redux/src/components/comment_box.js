13ae57cfffccc0cbc0dd4472c5e52f6723df5ad9
nichenqin1001 <nichenqin@hotmail.com>
2017-05-11 09:50:49 +0800

    comment box test

--- React/Advanced_React_And_Redux/src/components/comment_box.js
+++ React/Advanced_React_And_Redux/src/components/comment_box.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 export default class CommentBox extends Component {
-    render() {
-        return (
-            <div className="comment-box">
-                <textarea />
-                <button>Submit Comment</button>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = { comment: '' };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({ comment: '' });
+  }
+
+  handleChange(e) {
+    this.setState({ comment: e.target.value });
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit.bind(this)}
+        className="comment-box">
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange.bind(this)}
+        />
+        <button>Submit Comment</button>
+      </form>
+    );
+  }
 }
--- React/Advanced_React_And_Redux/test/components/comment_box_test.js
+++ React/Advanced_React_And_Redux/test/components/comment_box_test.js
@@ -2,21 +2,39 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
-    let component;
+  let component;
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the correct class', () => {
+    expect(component).to.have.class('comment-box');
+  });
+
+  it('has a text area', () => {
+    expect(component.find('textarea')).to.exist;
+  });
+
+  it('has a button', () => {
+    expect(component.find('button')).to.exist;
+  });
+
+  describe('enter some text', () => {
     beforeEach(() => {
-        component = renderComponent(CommentBox);
+      component.find('textarea').simulate('change', 'new comment here');
     });
 
-    it('has the correct class', () => {
-        expect(component).to.have.class('comment-box')
-    })
-
-    it('has a text area', () => {
-        expect(component.find('textarea')).to.exist;
+    it('shows text is entered', () => {
+      expect(component.find('textarea')).to.have.value('new comment here');
     });
 
-    it('has a button', () => {
-        expect(component.find('button')).to.exist;
+    it('when submitted, clears the input', () => {
+      // component is the <form>HTLMElment
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
     });
+  });
+
+
 
 });
