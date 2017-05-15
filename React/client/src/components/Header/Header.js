29e385b3ee3b2d051d02fd0058509565b0a40515
nichenqin1001 <nichenqin@hotmail.com>
2017-05-15 11:39:51 +0800

    header logic

--- React/client/src/components/Header/Header.js
+++ React/client/src/components/Header/Header.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
-export default () => (
-  <nav className="navbar navbar-default">
-    <div className="container">
-      <button type="button" className="btn btn-success navbar-btn pull-right">Sign in</button>
-    </div>
-  </nav>
-);
+class Header extends Component {
+  renderLinks() {
+    const { authenticated } = this.props;
+
+    const btnClass = classnames('btn', 'navbar-btn', 'pull-right', {
+      'btn-warning': authenticated,
+      'btn-success': !authenticated
+    });
+
+    if (authenticated) {
+      return <Link className={btnClass} to="/signout">Sigh Out</Link>;
+    } else {
+      return [
+        <Link className={btnClass} to="/signin">Sigh In</Link>,
+        <Link className={btnClass} to="/signup">Sigh Up</Link>];
+    }
+
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container">
+          <Link to="/" className="navbar-brand">Redux Auth</Link>
+          {this.renderLinks()}
+        </div>
+      </nav>
+    );
+  }
+}
+
+Header = connect(({ auth: { authenticated } }) => ({ authenticated }))(Header);
+
+export default Header;
