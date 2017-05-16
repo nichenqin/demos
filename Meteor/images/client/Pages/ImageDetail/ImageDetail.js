1d5ecb19e5a4e8b7c005690c2c9aab91c724d275
nichenqin1001 <nichenqin@hotmail.com>
2017-05-16 11:57:15 +0800

    styling

--- Meteor/images/client/Pages/ImageDetail/ImageDetail.js
+++ Meteor/images/client/Pages/ImageDetail/ImageDetail.js
@@ -4,8 +4,16 @@ class ImageDetail extends Component {
     render() {
         return (
             <li>
-                <img src={this.props.image.link} alt="" />
-                {this.props.image.title}
+                <div className="media list-group-item">
+                    <div className="media-left">
+                        <img src={this.props.image.link} alt="" />
+                    </div>
+                    <div className="media-body">
+                        <h4 className="media-heading">
+                            {this.props.image.title}
+                        </h4>
+                    </div>
+                </div>
             </li>
         );
     }
--- Meteor/images/client/Pages/ImageList/ImageList.js
+++ Meteor/images/client/Pages/ImageList/ImageList.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 import ImageDetail from "../ImageDetail/ImageDetail";
 
 const images = [
-    { title: "Pen", link: "https://dummyimage.com/600x400" },
-    { title: "Ping Tree", link: "https://dummyimage.com/600x400" },
-    { title: "Mug", link: "https://dummyimage.com/600x400" }
+    { title: "Pen", link: "https://dummyimage.com/64x64" },
+    { title: "Ping Tree", link: "https://dummyimage.com/64x64" },
+    { title: "Mug", link: "https://dummyimage.com/64x64" }
 ];
 
 class ImageList extends Component {
     renderImages() {
-        return images.map(image => <ImageDetail image={image} />);
+        return images.map(image => <ImageDetail key={image.title} image={image} />);
     }
 
     render() {
         return (
-            <ul>
+            <ul className="media-list list-group">
                 {this.renderImages()}
             </ul>
         );
