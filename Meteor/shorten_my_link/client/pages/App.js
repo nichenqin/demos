f7ce9b9951daac7c21cb5dc190989751dee24a79
nichenqin1001 <nichenqin@hotmail.com>
2017-05-17 10:00:56 +0800

    init react

---
+++ Meteor/shorten_my_link/client/pages/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class APP extends Component {
+  render() {
+    return (
+      <div>
+        App Runs
+      </div>
+    );
+  }
+}
+
+export default APP;
