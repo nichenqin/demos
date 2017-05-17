f54bd4030fe52bfd33eedb254e3e645a30585eb1
nichenqin1001 <nichenqin@hotmail.com>
2017-05-17 11:50:25 +0800

    validate methods

--- Meteor/shorten_my_link/client/pages/Links/LinkCreat.js
+++ Meteor/shorten_my_link/client/pages/Links/LinkCreat.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: '' };
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    console.log(this.refs.input.value);
+    const { input } = this.refs;
+
+    Meteor.call('links.insert', input.value, error => {
+      this.setState({ error: error ? 'Enter a Valid URL' : '' });
+      input.value = error ? input.value : '';
+    });
   }
 
   render() {
+    const { error } = this.state;
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <label>Links to Shorten</label>
           <input ref="input" type="text" className="form-control" />
         </div>
+        {error && <div className="alert alert-danger">{error}</div>}
         <button className="btn btn-primary">Shorten</button>
       </form>
     );
--- Meteor/shorten_my_link/imports/collections/links.js
+++ Meteor/shorten_my_link/imports/collections/links.js
@@ -1,3 +1,11 @@
-import { Mongo } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { isUri } from 'valid-url';
+import { check, Match } from 'meteor/check';
+
+Meteor.methods({
+  'links.insert': url => {
+    check(url, Match.Where(url => isUri(url)));
+  }
+});
 
 export const LinkCollection = new Mongo.Collection('links');
