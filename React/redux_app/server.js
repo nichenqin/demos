820d468d337b452330743db75caf32a8161e7044
nichenqin1001 <nichenqin@hotmail.com>
2017-05-18 12:19:15 +0800

    init project

---
+++ React/redux_app/server.js
@@ -0,0 +1,14 @@
+"use strict"
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.senFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+
+app.listen(3000, function () {
+  console.log('> listening at http://localhost:3000')
+});
