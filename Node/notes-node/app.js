cf6f8a682910b7b944aee13da289d7c7e26d1af5
nichenqin1001 <nichenqin@hotmail.com>
2017-06-02 11:47:56 +0800

    fs module

---
+++ Node/notes-node/app.js
@@ -0,0 +1,9 @@
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user);
+
+// fs.appendFileSync('greeting.txt', 'hello world!');
