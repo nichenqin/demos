293cdae897b65effa825ddf5323d58e91c0019ad
nichenqin1001 <nichenqin@hotmail.com>
2017-07-11 11:29:32 +0800

    setting css class

--- vue/firstVueJsApp/main.js
+++ vue/firstVueJsApp/main.js
@@ -1,11 +1,17 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello world'
+    title: 'Hello world',
+    link: 'http://www.google.com',
+    finishedLink: '<a href="http://www.google.com" >Google</a>'
   },
   methods: {
     changeTitle: function (event) {
       this.title = event.target.value;
+    },
+    sayHello: function () {
+      this.title = 'Hello!';
+      return this.title;
     }
   }
 })
