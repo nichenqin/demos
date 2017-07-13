7922ccbb5dcaf5688a18c07cccadce0b47be10e8
nichenqin1001 <nichenqin@hotmail.com>
2017-07-13 15:59:22 +0800

    slot

---
+++ vue/quote/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>Quote</h2>
+          <p>A Wonderful Quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+export default {
+  components: {
+    appQuote: Quote
+  }
+}
+</script>
+
+<style>
+
+</style>
