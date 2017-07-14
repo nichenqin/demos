983ffe455e3b158fe674921343e6d2b748d1808d
nichenqin1001 <nichenqin@hotmail.com>
2017-07-14 16:52:19 +0800

    local directive & modify

--- vue/directives/src/App.vue
+++ vue/directives/src/App.vue
@@ -12,6 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight.delayed="'red'">Color this</p>
+        <p v-local-highlight.delayed.blink="'red'">Color this local</p>
       </div>
     </div>
   </div>
@@ -19,7 +20,32 @@
 
 <script>
 export default {
-
+  directives: {
+    'local-highlight': {
+      bind(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers['delayed']) {
+          delay = 3000;
+        }
+        if (binding.modifiers['blink']) {
+          let mainColor = binding.value;
+          let subColor = 'blue';
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == subColor ? currentColor = mainColor : currentColor = subColor;
+              console.log(currentColor);
+              el.style.backgroundColor = currentColor;
+            }, 1000)
+          }, delay)
+        } else {
+          setTimeout(() => {
+            el.style.backgroundColor = binding.value;
+          }, delay)
+        }
+      }
+    }
+  }
 }
 </script>
 
