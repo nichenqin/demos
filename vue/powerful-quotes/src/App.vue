ef31054ca2f4d1998807f96ac73a614765048f29
nichenqin1001 <nichenqin@hotmail.com>
2017-07-14 10:52:32 +0800

    delete quote

--- vue/powerful-quotes/src/App.vue
+++ vue/powerful-quotes/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid @quoteDelete="deleteQuote" :quotes="quotes"></app-quote-grid>
     <div class="row">
       <div class="col-cm-12 text-center">
         <div class="alert alert-info">Info: Click on the quote to delete it!</div>
@@ -13,6 +14,8 @@
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
 import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
 export default {
   data() {
     return {
@@ -22,11 +25,16 @@ export default {
   },
   components: {
     appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote
+    appNewQuote: NewQuote,
+    appHeader: Header
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length >= 10) return alert('Please Delete Quote First!');
       quote.length && this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
     }
   }
 }
