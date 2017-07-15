a231a635d71c4a1dcc916030e4a2c3426e74bd63
nichenqin1001 <nichenqin@hotmail.com>
2017-07-15 17:52:02 +0800

    understanding javascript animation hook

--- vue/animations/src/App.vue
+++ vue/animations/src/App.vue
@@ -18,9 +18,16 @@
           <div v-if="show" class="alert alert-info">some slide info</div>
         </transition>
         <transition name="fade" appear mode="out-in">
-          <!--only one element inside this transirion  -->
+          <!--only one element inside this transition  -->
           <div v-if="show" class="alert alert-info" key="info">some slide info</div>
           <div v-else class="alert alert-warning" key="warning">some slide warning info</div>
+        </transition>
+        <hr>
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br>
+        <br>
+        <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter" @enter-cancelled="enterCancelled" @before-leave="beforeLeave" @leave="leave" @after-leave="afterLeave" @leave-cancelled="leaveCancelled" :css="false">
+          <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
         </transition>
       </div>
     </div>
@@ -32,8 +39,49 @@ export default {
   data() {
     return {
       show: false,
-      alertAnimation: 'fade'
+      load: true,
+      alertAnimation: 'fade',
+      elementWidth: 300
     }
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + 'px';
+    },
+    enter(el, done) {
+      console.log('enter');
+      const interval = setInterval(() => {
+        this.elementWidth++;
+        el.style.width = this.elementWidth + 'px';
+        if (+this.elementWidth >= 300) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20)
+      done();
+    },
+    afterEnter(el) { console.log('after enter'); },
+    enterCancelled(el) { console.log('enter cancelled'); },
+    beforeLeave(el) {
+      console.log('before leave');
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + 'px';
+    },
+    leave(el, done) {
+      console.log('leave');
+      const interval = setInterval(() => {
+        this.elementWidth--;
+        el.style.width = this.elementWidth + 'px';
+        if (this.elementWidth <= 100) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20)
+    },
+    afterLeave(el) { console.log('after leave'); },
+    leaveCancelled(el) { console.log('leave cancelled'); }
   }
 }
 </script>
