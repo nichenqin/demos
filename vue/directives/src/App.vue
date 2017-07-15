e9fdaf938c876205b0580b34bb2eef78744c6f50
nichenqin1001 <nichenqin@hotmail.com>
2017-07-15 09:47:27 +0800

    use more complex directive value

--- vue/directives/src/App.vue
+++ vue/directives/src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight.delayed="'red'">Color this</p>
-        <p v-local-highlight.delayed.blink="'red'">Color this local</p>
+        <p v-local-highlight.delayed.blink="{mainColor: 'red', subColor: 'green', delay: 500}">Color this local</p>
       </div>
     </div>
   </div>
@@ -28,8 +28,7 @@ export default {
           delay = 3000;
         }
         if (binding.modifiers['blink']) {
-          let mainColor = binding.value;
-          let subColor = 'blue';
+          const { mainColor, subColor } = binding.value;
           let currentColor = mainColor;
           setTimeout(() => {
             setInterval(() => {
