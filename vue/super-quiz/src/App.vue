84d6c76ecb98bbcfc71e182113da319708b8d506
nichenqin1001 <nichenqin@hotmail.com>
2017-07-15 23:25:24 +0800

    finish super quiz logic

---
+++ vue/super-quiz/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-md-offset-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-md-offset-3">
+        <component :is="currentComponent" @answered="answered($event)" @comfirmed="isRightAnswer = false"></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Answer from './components/Answer.vue';
+import Question from './components/Question.vue';
+
+export default {
+  data() {
+    return {
+      isRightAnswer: false
+    }
+  },
+  methods: {
+    answered(isCorrect) {
+      this.isRightAnswer = isCorrect;
+    }
+  },
+  computed: {
+    currentComponent() {
+      return this.isRightAnswer ? 'app-answer' : 'app-question';
+    }
+  },
+  components: {
+    appAnswer: Answer,
+    appQuestion: Question
+  }
+}
+</script>
---
+++ vue/super-quiz/src/components/Answer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="alert alert-success text-center">
+    <h1>That's correct</h1>
+    <hr>
+    <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onNextQuestion() {
+      this.$emit('comfirmed');
+    }
+  }
+}
+</script>
