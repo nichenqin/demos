2c8acf13735aa97fc6087fe40bb68267b9e35164
nichenqin1001 <nichenqin@hotmail.com>
2017-07-16 16:59:05 +0800

    understanding vue resource

--- vue/vue-resource/src/App.vue
+++ vue/vue-resource/src/App.vue
@@ -12,24 +12,46 @@
           <input type="email" class="form-control" v-model="user.email">
         </div>
         <button class="btn btn-primary" @click.prevent="onSubmit">submit</button>
+        <hr>
+        <button class="btn btn-success" @click="fetchData">fetch data</button>
+        <ul class="list-group">
+          <li class="list-group-item" v-for="user in users" :key="user.userName">{{user.userName}} / {{user.email}}</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const url = 'https://vue-resource-6724f.firebaseio.com/';
+
 export default {
   data() {
     return {
       user: {
         userName: '',
         email: ''
-      }
+      },
+      users: []
     }
   },
   methods: {
     onSubmit() {
-      console.log(this.user);
+      this.$http
+        .post(`${url}data.json`, this.user)
+        .then(response => { console.log(response); });
+    },
+    fetchData() {
+      this.$http
+        .get(`${url}data.json`)
+        .then(response => response.json())
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
     }
   }
 }
