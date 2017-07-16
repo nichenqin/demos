1e0fa55f4dda40130f58100ba867df293a554752
nichenqin1001 <nichenqin@hotmail.com>
2017-07-16 14:15:37 +0800

    setup vue http module & use firebase as service

---
+++ vue/vue-resource/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-md-6 col-md-offset-3">
+        <h1>Vue Resource</h1>
+        <div class="form-group">
+          <label for="">Username</label>
+          <input type="text" class="form-control" v-model="user.userName">
+        </div>
+        <div class="form-group">
+          <label for="">Email</label>
+          <input type="email" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click.prevent="onSubmit">submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        userName: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    }
+  }
+}
+</script>
