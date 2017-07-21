75357b0e6ea701381bd86db119b541e69ec200f3
nichenqin1001 <nichenqin@hotmail.com>
2017-07-21 23:31:08 +0800

    save & delete with ajax

--- Express/index.js
+++ Express/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const engines = require("consolidate");
 
@@ -14,12 +16,24 @@ fs.readFile("users.json", { encoding: "utf8" }, (err, data) => {
   });
 });
 
+const getUserFilePath = username =>
+  path.join(__dirname, "user", username) + ".json";
+
+const getUser = username => users.find(user => user.username === username);
+
+const saveUser = (username, data) => {
+  const fp = getUserFilePath(username);
+  fs.unlinkSync(fp);
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), { encoding: "utf8" });
+};
+
 app.engine("hbs", engines.handlebars);
 
 app.set("views", "./views");
 app.set("view engine", "hbs");
 
 app.use("/profilepics", express.static("images"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { users });
@@ -27,7 +41,18 @@ app.get("/", (req, res) => {
 
 app.get("/:username", (req, res) => {
   const { username } = req.params;
-  res.render("user", { username });
+  const user = getUser(username);
+  const { location } = user;
+
+  res.render("user", { user, username, location });
+});
+
+app.put("/:username", (req, res) => {
+  const { username } = req.param;
+  const user = getUser(username);
+  user.location = req.body;
+  saveUser(username, user);
+  res.end();
 });
 
 const server = app.listen(3000, () => {
