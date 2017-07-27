3227b004bca344b0e45b94c547bacb4ada1db7a2
nichenqin1001 <nichenqin@hotmail.com>
2017-07-27 23:19:31 +0800

    render todo html

--- Koa/TodoList/index.js
+++ Koa/TodoList/index.js
@@ -5,11 +5,37 @@ const views = require("koa-views");
 const app = new Koa();
 const router = new Router();
 
+const todos = [
+  { id: 1, content: "todo1", done: false },
+  { id: 2, content: "todo2", done: false },
+  { id: 3, content: "todo3", done: false }
+];
+
 app.use(views(__dirname + "/views", { extension: "handlebars" }));
 
-router.get("/", async ctx => {
-  await ctx.render("index", { title: "hello world" });
-});
+router
+  .get("/", async ctx => {
+    await ctx.render("index", { todos });
+  })
+  .get("/:id", async ctx => {
+    const { id } = ctx.params;
+    const todo = todos.find(item => item.id === +id);
+    console.log(todo);
+    await ctx.render("todo", { todo });
+  })
+  .post("/", async (ctx, next) => {
+    const { id, content, done } = ctx.query;
+    todos.push({
+      id: id || Date.now(),
+      content,
+      done: done || false
+    });
+    await next();
+    ctx.redirect("/");
+  })
+  .delete("/:id", ctx => {
+    console.log(ctx.params);
+  });
 
 app.use(router.routes()).use(router.allowedMethods());
 
