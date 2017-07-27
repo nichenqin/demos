e8ec6de243dd89307aed06290ec2441f306c2b8d
nichenqin1001 <nichenqin@hotmail.com>
2017-07-27 15:47:48 +0800

    setup passport google strategy

--- NodeReactFullStack/server/index.js
+++ NodeReactFullStack/server/index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const { googleClientID, googleClientSecret } = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send({ hi: "thers" });
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 app.listen(PORT, () => {
   console.log(">> Listening at port: " + PORT);
