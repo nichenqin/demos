6bd87c492d3ba03804cd1264a1fc11f8b3dfd1da
nichenqin1001 <nichenqin@hotmail.com>
2017-07-28 17:34:53 +0800

    reconstructoe folders

--- NodeReactFullStack/server/index.js
+++ NodeReactFullStack/server/index.js
@@ -1,35 +1,10 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const { googleClientID, googleClientSecret } = require("./config/keys");
+require("./service/passport");
 
 const app = express();
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: googleClientID,
-      clientSecret: googleClientSecret,
-      callbackURL: "auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
+require("./routes/authRoute")(app);
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send({ hi: "thers" });
-});
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
 
 app.listen(PORT, () => {
   console.log(">> Listening at port: " + PORT);
