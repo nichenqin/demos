4265d94a2122cbd507ba40d39a6d4718173b850e
nichenqin <nichenqin@hotmail.com>
2017-09-15 17:32:27 +0800

    finish proxy function

--- vue/ReVue/src/Vue.js
+++ vue/ReVue/src/Vue.js
@@ -1,6 +1,6 @@
 class Vue {
   constructor(option) {
-    this.$date = option.data || {};
+    this.$data = option.data || {};
     this.$el =
       typeof option.el === "string"
         ? document.querySelector(option.el)
@@ -12,7 +12,39 @@ class Vue {
       ...option
     };
 
+    this._proxy(option);
+
     this.$option = option;
+  }
+
+  _proxy(option) {
+    const proxyList = ["data", "computed"];
+    proxyList.forEach(proxy => {
+      Object.keys(Reflect.get(option, proxy)).forEach(key => {
+        Reflect.defineProperty(this, key, {
+          configurable: true,
+          enumerable: true,
+          get() {
+            if (typeof Reflect.get(this.$data, key) !== "undefined") {
+              return Reflect.get(this.$data, key);
+            } else if (
+              typeof Reflect.get(this.$option.computed, key) !== "undefined"
+            ) {
+              return Reflect.get(this.$option.computed, key);
+            } else {
+              return undefined;
+            }
+          },
+          set(newValue) {
+            if (this.$data.hasOwnProperty(key)) {
+              Reflect.set(this.$data, key, newValue);
+            } else if (this.$option.computed.hasOwnProperty(key)) {
+              Reflect.set(this.$option.computed, key, newValue);
+            }
+          }
+        });
+      });
+    });
   }
 }
 
--- vue/ReVue/src/index.js
+++ vue/ReVue/src/index.js
@@ -3,7 +3,12 @@ import Vue from "./Vue.js";
 const vm = new Vue({
   el: "#app",
   data: {
-    msg: "hello word"
+    msg: "hello word",
+    text: "iam a string",
+    obj: {
+      key: "value"
+    }
   }
 });
 console.log(vm);
+console.log(vm.msg);
