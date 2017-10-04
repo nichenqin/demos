738d8a3547096c05d28f588dce26d9d7d0d2cfd8
nichenqin <nichenqin@hotmail.com>
2017-10-04 14:03:10 +0800

    set up mongoose & idea model

--- NodeExpressMongoDB/vidjot/app.js
+++ NodeExpressMongoDB/vidjot/app.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 const port = 5000;
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect("mongodb://localhost/vidjot-dev", {
+    useMongoClient: true
+  })
+  .then(() => {
+    console.log("MongoDB Connected...");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+require("./models/idea");
+const Idea = mongoose.model("ideas");
 
 // handlerbars middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
