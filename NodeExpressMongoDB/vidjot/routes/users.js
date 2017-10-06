32d9b6fc87eb17546e83e18168bd78c10096e648
nichenqin <nichenqin@hotmail.com>
2017-10-06 23:03:14 +0800

    register validator

--- NodeExpressMongoDB/vidjot/routes/users.js
+++ NodeExpressMongoDB/vidjot/routes/users.js
@@ -10,4 +10,31 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  const errors = [];
+
+  if (password !== password2) {
+    errors.push({ msg: "Password not match" });
+  }
+
+  if (password.length < 4) {
+    errors.push({ msg: "Password should longer than 4" });
+  }
+
+  if (errors.length) {
+    res.render("users/register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("passed");
+  }
+
+  res.render("users/register");
+});
+
 module.exports = router;
